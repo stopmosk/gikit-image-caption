65d5aaf8095645f7e24ff8aa0dfea18c048e8793
Sergei Shutov <sergei.shutov@compvisionsys.com>
2021-05-05 12:47:03 +0300

    BertImgModelOCR calss Created

--- oscar/modeling/modeling_bert.py
+++ oscar/modeling/modeling_bert.py
@@ -133,14 +133,126 @@ class CaptionBertLayer(BertLayer):
         self.intermediate = BertIntermediate(config)
         self.output = BertOutput(config)
 
-    def forward(self, hidden_states, attention_mask, head_mask=None,
-                history_state=None):
-        attention_outputs = self.attention(hidden_states, attention_mask,
-                head_mask, history_state)
+    def forward(self, hidden_states, attention_mask, head_mask=None, history_state=None):
+        attention_outputs = self.attention(hidden_states, attention_mask, head_mask, history_state)
         attention_output = attention_outputs[0]
         intermediate_output = self.intermediate(attention_output)
         layer_output = self.output(intermediate_output, attention_output)
         outputs = (layer_output,) + attention_outputs[1:]  # add attentions if we output them
+        return outputs
+
+
+class BertImgModelOCR(BertPreTrainedModel):
+    """ Expand from BertModel to handle image region features as input
+    """
+    def __init__(self, config):
+        super(BertImgModel, self).__init__(config)
+        self.embeddings = BertEmbeddings(config)
+        self.encoder = CaptionBertEncoder(config)
+        self.pooler = BertPooler(config)   # tanh(Linear(bert_first_out_token))
+
+        self.ocr_dim = config.ocr_dim
+        self.ocr_embedding = nn.Linear(self.ocr_dim, self.config.hidden_size, bias=True)  # stopmosk
+
+        self.img_dim = config.img_feature_dim
+        logger.info(f'BertImgModel Image Dimension: {self.img_dim}')
+        self.img_feature_type = config.img_feature_type
+        self.use_img_layernorm = config.use_img_layernorm if hasattr(config, 'use_img_layernorm') else None
+
+        # img_feature_type is 'frcnn' Faster R-CNN
+        self.img_embedding = nn.Linear(self.img_dim, self.config.hidden_size, bias=True)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        if self.use_img_layernorm:
+            self.LayerNorm = BertLayerNorm(config.hidden_size, eps=config.img_layer_norm_eps)
+
+        self.apply(self.init_weights)
+
+    def _resize_token_embeddings(self, new_num_tokens):
+        old_embeddings = self.embeddings.word_embeddings
+        new_embeddings = self._get_resized_embeddings(old_embeddings, new_num_tokens)
+        self.embeddings.word_embeddings = new_embeddings
+        return self.embeddings.word_embeddings
+
+    def _prune_heads(self, heads_to_prune):
+        """ Prunes heads of the model.
+            heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
+            See base class PreTrainedModel
+        """
+        for layer, heads in heads_to_prune.items():
+            self.encoder.layer[layer].attention.prune_heads(heads)
+
+    def forward(self, input_ids, img_feats=None, token_type_ids=None, attention_mask=None,
+                position_ids=None, head_mask=None, encoder_history_states=None):
+        if attention_mask is None:
+            attention_mask = torch.ones_like(input_ids)
+
+        if token_type_ids is None:
+            token_type_ids = torch.zeros_like(input_ids)
+
+        # We create a 3D attention mask from a 2D tensor mask.
+        # Sizes are [batch_size, 1, 1, to_seq_length]
+        # So we can broadcast to [batch_size, num_heads, from_seq_length, to_seq_length]
+        # this attention mask is more simple than the triangular masking of causal attention
+        # used in OpenAI GPT, we just need to prepare the broadcast dimension here.
+        if attention_mask.dim() == 2:
+            extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(2)
+        elif attention_mask.dim() == 3:
+            extended_attention_mask = attention_mask.unsqueeze(1)
+        else:
+            raise NotImplementedError
+
+        # Since attention_mask is 1.0 for positions we want to attend and 0.0 for
+        # masked positions, this operation will create a tensor which is 0.0 for
+        # positions we want to attend and -10000.0 for masked positions.
+        # Since we are adding it to the raw scores before the softmax, this is
+        # effectively the same as removing these entirely.
+        extended_attention_mask = extended_attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        # Prepare head mask if needed
+        # 1.0 in head_mask indicate we keep the head
+        # attention_probs has shape bsz x n_heads x N x N
+        # input head_mask has shape [num_heads] or [num_hidden_layers x num_heads]
+        # and head_mask is converted to shape [num_hidden_layers x batch x num_heads x seq_length x seq_length]
+        if head_mask is not None:
+            if head_mask.dim() == 1:
+                head_mask = head_mask.unsqueeze(0).unsqueeze(0).unsqueeze(-1).unsqueeze(-1)
+                head_mask = head_mask.expand(self.config.num_hidden_layers, -1, -1, -1, -1)
+            elif head_mask.dim() == 2:
+                head_mask = head_mask.unsqueeze(1).unsqueeze(-1).unsqueeze(-1)  # We can specify head_mask for each layer
+            # switch to float if needed + fp16 compatibility
+            head_mask = head_mask.to(dtype=next(self.parameters()).dtype) # switch to fload if need + fp16 compatibility
+        else:
+            head_mask = [None] * self.config.num_hidden_layers
+
+        embedding_output = self.embeddings(input_ids, position_ids=position_ids, token_type_ids=token_type_ids)
+        if encoder_history_states:
+            assert img_feats is None, "Cannot take image features while using encoder history states"
+
+        # ocr_embedding_output = self.ocr_embedding(ocr_feats)
+
+        if img_feats is not None:
+            # frcnn features
+            img_embedding_output = self.img_embedding(img_feats)
+            if self.use_img_layernorm:
+                img_embedding_output = self.LayerNorm(img_embedding_output)
+            # add dropout on image embedding
+            img_embedding_output = self.dropout(img_embedding_output)
+            # concatenate two embeddings
+            embedding_output = torch.cat((embedding_output, img_embedding_output), 1)
+
+        encoder_outputs = self.encoder(
+            embedding_output,
+            extended_attention_mask,
+            head_mask=head_mask,
+            encoder_history_states=encoder_history_states
+        )
+
+        sequence_output = encoder_outputs[0]
+        pooled_output = self.pooler(sequence_output)
+
+        # add hidden_states and attentions if they are here
+        outputs = (sequence_output, pooled_output,) + encoder_outputs[1:]
         return outputs
 
 
@@ -169,6 +281,7 @@ class BertImgModel(BertPreTrainedModel):
             self.code_embeddings = nn.Embedding(config.code_voc, config.code_dim, padding_idx=0)
             self.img_embedding = nn.Linear(config.code_dim, self.config.hidden_size, bias=True)
         else:
+            # img_feature_type is 'frcnn' Faster R-CNN
             self.img_embedding = nn.Linear(self.img_dim, self.config.hidden_size, bias=True)
             self.dropout = nn.Dropout(config.hidden_dropout_prob)
             if self.use_img_layernorm:
@@ -250,6 +363,7 @@ class BertImgModel(BertPreTrainedModel):
                 code_emb = self.code_embeddings(img_feats)
                 img_embedding_output = self.img_embedding(code_emb)
             else:
+                # frcnn features
                 img_embedding_output = self.img_embedding(img_feats)
                 if self.use_img_layernorm:
                     img_embedding_output = self.LayerNorm(img_embedding_output)
