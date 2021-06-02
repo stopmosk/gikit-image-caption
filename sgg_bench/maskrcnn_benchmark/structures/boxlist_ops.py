8bf805364732243264a01abf5d52da701b766952
stopmosk <serg.shutov@gmail.com>
2021-06-02 19:00:46 +0300

    Sync from dl2

--- sgg_bench/maskrcnn_benchmark/structures/boxlist_ops.py
+++ sgg_bench/maskrcnn_benchmark/structures/boxlist_ops.py
@@ -43,9 +43,7 @@ def remove_small_boxes(boxlist, min_size):
     # TODO maybe add an API for querying the ws / hs
     xywh_boxes = boxlist.convert("xywh").bbox
     _, _, ws, hs = xywh_boxes.unbind(dim=1)
-    keep = (
-        (ws >= min_size) & (hs >= min_size)
-    ).nonzero().squeeze(1)
+    keep = ((ws >= min_size) & (hs >= min_size)).nonzero(as_tuple=False).squeeze(1)
     return boxlist[keep]
 
 
--- train_ce_2080_textcaps.sh
+++ train_ce_2080_textcaps.sh
@@ -5,13 +5,14 @@ python oscar/run_captioning.py \
 --do_train \
 --evaluate_during_training \
 --data_dir=../datasets/textcaps \
---model_name_or_path=../output/scst/checkpoint-31-896000 \
+--model_name_or_path=../output/txtcps_xe_clro_posenc_roz_pos/checkpoint-78-204000 \
 --do_lower_case \
 --add_od_labels \
 --add_ocr_labels \
---learning_rate=0.00001 \
---per_gpu_train_batch_size=60 \
---per_gpu_eval_batch_size=100 \
+--max_ocr_seq_length=20 \
+--learning_rate=0.00005 \
+--per_gpu_train_batch_size=48 \
+--per_gpu_eval_batch_size=90 \
 --gradient_accumulation_steps=1 \
 --num_train_epochs=80 \
 --num_workers=6 \
@@ -21,7 +22,7 @@ python oscar/run_captioning.py \
 --label_smoothing=0.1 \
 --drop_worst_ratio=0.2 \
 --drop_worst_after=20000 \
---logging_steps=100 \
+--logging_steps=200 \
 --save_steps=2000 \
---output_dir=../output/txtcps_base_xe_rosetta02/ \
+--output_dir=../output/txtcps_xe_clro_posenc_roz_pos_xywh/ \
 --fp16
