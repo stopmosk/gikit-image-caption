11eb8417c79f0e7e6dd094dc9194db4933913fe9
Sergei Shutov <serg.shutov@gmail.com>
2021-06-25 12:38:08 +0300

    Sync

--- dl3_eval.sh
+++ dl3_eval.sh
@@ -1,9 +1,9 @@
 python oscar/run_captioning.py \
 --do_eval \
---data_dir=../datasets/coco/coco_oscar_vvl_my \
+--data_dir=../datasets/coco/coco_my_oscar_vvl \
 --per_gpu_eval_batch_size=120 \
 --num_workers=6 \
 --num_beams=1 \
 --max_gen_length=20 \
---eval_model_dir=../output/coco/checkpoint-25-76000 \
+--eval_model_dir=../pretrained_models/image_captioning/coco_captioning_base_xe/checkpoint-60-66360 \
 --fp16
