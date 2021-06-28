1ad009e8e6274547c19ec6ef3dc7f68f643877b4
Sergei Shutov <sergei.shutov@compvisionsys.com>
2021-06-28 11:18:33 +0300

    Sync

--- eval_1060_my1.sh
+++ eval_1060_my1.sh
@@ -4,10 +4,10 @@
 
 python oscar/run_captioning.py \
 --do_eval \
---data_dir=../datasets/coco_caption \
+--data_dir=../datasets/coco/coco_oscar_preexacted_vinvl \
 --per_gpu_eval_batch_size=32 \
 --num_workers=4 \
 --num_beams=1 \
 --max_gen_length=20 \
---output_dir=../output/scst_results \
---eval_model_dir=../output/scst/checkpoint-2-74000
+--output_dir=../output/tmp_results \
+--eval_model_dir=../output/coco_my_oscar_vvl_!/checkpoint-1-10000
