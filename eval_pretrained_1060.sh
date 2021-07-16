890b89853bb8e1f04e6e4a6e24bfb111b6330354
Sergei Shutov <sergei.shutov@compvisionsys.com>
2021-07-16 11:42:20 +0300

    Sync

--- eval_pretrained_1060.sh
+++ eval_pretrained_1060.sh
@@ -4,10 +4,11 @@
 
 python oscar/run_captioning.py \
 --do_eval \
---add_ocr_labels \
---data_dir=../datasets/coco_caption \
---per_gpu_eval_batch_size=1 \
+--do_test \
+--data_dir=../datasets/coco/coco_oscar_preexacted_vinvl \
+--per_gpu_eval_batch_size=16 \
 --num_workers=4 \
---num_beams=1 \
+--num_beams=5 \
 --max_gen_length=20 \
---eval_model_dir=../pretrained_models/image_captioning/coco_captioning_base_xe/checkpoint-60-66360
+--eval_model_dir=../output/scst/checkpoint-22-628000
+
