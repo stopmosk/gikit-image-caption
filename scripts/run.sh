cdb8f1b4c6a0906012b75ab396409d8ffeb20809
stopmosk <serg.shutov@gmail.com>
2021-10-28 09:59:53 +0300

    Sync

--- scripts/run.sh
+++ scripts/run.sh
@@ -23,6 +23,9 @@
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/agg_1cc_10coco_20huawei_base_xe1/checkpoint-5-550000
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_1000 ../output/agg_1cc_10coco_20huawei_base_xe1/checkpoint-5-550000
 
+bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../models/checkpoint-24-23000
+bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_1000 ../models/checkpoint-24-23000
+
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_1000 ../output/cc_base_xe/checkpoint-3-66000
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe/checkpoint-3-66000
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe/checkpoint-7-160000
@@ -31,8 +34,8 @@
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe/checkpoint-1-40000
 #bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe/checkpoint-2-60000
 
-bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe_2/checkpoint-15-200000
-bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_1000 ../output/cc_base_xe_2/checkpoint-15-200000
+#bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_5 ../output/cc_base_xe_2/checkpoint-15-200000
+#bash scripts/inference_oscar_vvl.sh ../datasets_orig/huawei_1000 ../output/cc_base_xe_2/checkpoint-15-200000
 
 #--eval_model_dir=../models/checkpoint-13-256000
 #--eval_model_dir=../models/checkpoint-22-628000
