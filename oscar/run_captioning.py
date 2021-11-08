69cdcb7a0f2261a1d2acc2ab4909c2b118903eab
stopmosk <serg.shutov@gmail.com>
2021-11-08 15:55:39 +0300

    Add multilang scripts and code

--- oscar/run_captioning.py
+++ oscar/run_captioning.py
@@ -1224,7 +1224,7 @@ def test(args, test_dataloader, model, tokenizer, predict_file):
 
                     # print(res)
 
-                    yield img_key, json.dumps(res)
+                    yield img_key, json.dumps(res, ensure_ascii=False)
 
         logger.info(f'Inference model computing time: {time_meter / (step+1)} seconds per batch')
 
