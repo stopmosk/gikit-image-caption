6c44b85587da7e3809b7a8e13d5ff165007eb26e
Sergei Shutov <sergei.shutov@compvisionsys.com>
2021-12-01 00:55:02 +0300

    Preinstall updates

--- pre_install.sh
+++ pre_install.sh
@@ -2,17 +2,20 @@
 
 CUR_DIR=$PWD
 
-sudo apt update && sudo apt install unzip ffmpeg libsm6 libxext6 &&
+sudo apt update && sudo apt install unzip aria2 ffmpeg libsm6 libxext6 &&
 
 pip install gdown &&
 
 cd .. &&
 if [ ! -f ./models.tar.gz ]; then
-    gdown https://drive.google.com/uc?id=19rI7ARKZAuhBUDIR-kFq3xTwcNa5yJn_
+    gdown https://drive.google.com/uc?id=1_PqHZkeGc1zEsgqU63uIcIJ8o2Q5qoEu
 fi &&
 
 mkdir -p models &&
-unzip models.zip -d ./models && cd $CUR_DIR &&
 #tar -xzvf models.tar.gz -C ./models && cd $CUR_DIR && 
+unzip models.zip -d ./models &&
+
+aria2c -x 16 -s 16 -d ./models https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.zip &&
+unzip wiki.en.zip -d ./models && cd $CUR_DIR &&
 
 cp -a ../models/cnmt_data/. CNMT/data
