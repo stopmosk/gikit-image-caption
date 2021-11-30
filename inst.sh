9acf321e45dac5dfc72e62b4eebb7dcbcf1ab037
stopmosk <serg.shutov@gmail.com>
2021-11-30 17:10:36 +0300

    Add EasyOCR

--- inst.sh
+++ inst.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-export MAX_JOBS=6
+export MAX_JOBS=4
+#sudo apt update && sudo apt install ffmpeg libsm6 libxext6
 
-conda create --name hic_pipe python=3.8 -y
-eval "$(conda shell.bash hook)"
-conda activate hic_pipe
+#conda create --name hic python=3.8 -y
+#eval "$(conda shell.bash hook)"
+#conda activate hic
 
 #conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
-#conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.2 -c pytorch -y
 conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch -y
 
 #Oscar
-cd huawei-image-caption
+cd BERT
 pip install -r requirements.txt
 #cd coco_caption && ./get_stanford_models.sh && cd ..
 #cd huawei-image-caption
-python setup.py build develop
+pip install -e .
 
 #VinVL
-#sudo apt update && sudo apt install ffmpeg libsm6 libxext6
 pip install ninja yacs==0.1.8 cityscapesscripts opencv-python
 conda install -c conda-forge pycocotools -y
 
-cd sgg_bench
-python setup.py build develop
-cd ..
+#SGG_Bench
+cd sgg_bench && pip install -e . && cd ../..
 
 # EasyOCR
-pip install easyocr
+cd EasyOCR && pip install -e . && cd ..
 
-# Translate
+# NMT
 conda install -c huggingface tokenizers=0.10.1 transformers=4.6.1 -y
 conda install sentencepiece -y
+
+# CNMT
+cd CNMT && pip install -e . && cd ..
