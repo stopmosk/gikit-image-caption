8c1ac2a5deeebab157f442d6041d5be144fac335
stopmosk <serg.shutov@gmail.com>
2021-12-01 01:35:26 +0300

    Update scripts

--- pre_install.sh
+++ pre_install.sh
@@ -4,18 +4,21 @@ CUR_DIR=$PWD
 
 sudo apt update && sudo apt install unzip aria2 ffmpeg libsm6 libxext6 &&
 
-pip install gdown &&
+pip install gdown && mkdir -p models &&
 
-cd .. &&
-if [ ! -f ./models.tar.gz ]; then
+cd ../models &&
+if [ ! -f ./models.zip ]; then
     gdown https://drive.google.com/uc?id=1_PqHZkeGc1zEsgqU63uIcIJ8o2Q5qoEu
 fi &&
 
-mkdir -p models &&
-#tar -xzvf models.tar.gz -C ./models && cd $CUR_DIR && 
-unzip models.zip -d ./models &&
+unzip models.zip &&
 
-aria2c -x 16 -s 16 -d ./models https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.zip &&
-unzip wiki.en.zip -d ./models && cd $CUR_DIR &&
+if [ ! -f ./wiki.en.zip ]; then
+    aria2c -x 16 -s 16 -d ./models https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.zip
+fi &&
+
+unzip wiki.en.zip && cd $CUR_DIR &&
 
 cp -a ../models/cnmt_data/. CNMT/data
+
+echo 'Done.'
